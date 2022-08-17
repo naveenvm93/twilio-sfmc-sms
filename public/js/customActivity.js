1a2108187aeef8ff59bf36c81c669b3bb07d887a
Naveen Murugan <naveen.murugan@sixt.com>
2022-08-17 15:56:52 +0200

    Config

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -100,7 +100,7 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            "to": "{{Contact.Attribute.TwilioV1.TwilioNumber}}" //<----This should map to your data extension name and phone number column
+            "to": "{{Contact.Attribute.SMSCustomActivity.MobileNumber}}" //<----This should map to your data extension name and phone number column
         }];
 
         payload['metaData'].isConfigured = true;
